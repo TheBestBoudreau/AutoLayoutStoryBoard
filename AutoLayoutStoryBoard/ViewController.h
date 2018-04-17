5c66837aa3483ba4251731df448cecccc440c977
Tyler Boudreau <boudreaut@hotmail.com>
2018-04-17 16:11:16 -0700

    Added some colourful shapes

---
+++ AutoLayoutStoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutStoryBoard
+//
+//  Created by Tyler Boudreau on 2018-04-17.
+//  Copyright © 2018 Tyler Boudreau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutStoryBoardTests/AutoLayoutStoryBoardTests.m
@@ -0,0 +1,39 @@
+//
+//  AutoLayoutStoryBoardTests.m
+//  AutoLayoutStoryBoardTests
+//
+//  Created by Tyler Boudreau on 2018-04-17.
+//  Copyright © 2018 Tyler Boudreau. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayoutStoryBoardTests : XCTestCase
+
+@end
+
+@implementation AutoLayoutStoryBoardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
